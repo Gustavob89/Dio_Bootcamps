12da394f9ea2f21cebd1deb4ef1c36be7bdd4bb1
Gustavob89 <76412104+Gustavob89@users.noreply.github.com>
2021-03-26 23:24:26 -0300

    Add files via upload

---
+++ CadastroSeries/CadastroSeries/Classes/EntidadeBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace CadastroSeries.Classes
+namespace CadastroSeries
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+
+    }
+}
