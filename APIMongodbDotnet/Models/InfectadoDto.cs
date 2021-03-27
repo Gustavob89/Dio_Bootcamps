18ffa70bab8c79a9a7e7eeb940a2fa1a46bd7063
Gustavob89 <76412104+Gustavob89@users.noreply.github.com>
2021-03-27 00:36:07 -0300

    Add files via upload

---
+++ APIMongodbDotnet/Models/InfectadoDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace APIMongodbDotnet.Models
+{
+    public class InfectadoDto
+    {
+         public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
